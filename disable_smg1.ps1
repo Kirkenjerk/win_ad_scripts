c550a39744adde78ea7888bb4a63fae77115ac7e
kuyker <kuyker@sage.edu>
2022-01-13 09:56:21 -0500

    Added SMB related script

---
+++ disable_smg1.ps1
@@ -0,0 +1,9 @@
+# Detect SMB1
+#Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+
+# Enabled SMB1
+#Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+
+
+# Disable SMB1
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
